e7e5aaaf9977bb98591ced0258849e8fe22fce96
Vinay-Pratap-Singh-Sengar <vinay.sengar17@gmail.com>
2026-08-12 11:55:52 +0530

    updated file

---
+++ class-02_Practice/program01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string fname;
+    string lname;
+
+    cout<<"Enter your first name ";
+    cin>> fname;
+
+    cout<<"Enter your last name ";
+    cin>> lname;
+
+    cout<<"User name is : "<<fname <<" "<<lname;
+    return 0;
+}
